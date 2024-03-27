ba3b624ec49e646b1474295f8cd63135d4c0f491
humay qasimova <hqasimova472@gmail.com>
2024-03-27 11:14:00 +0000

    demo

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ $(document).ready(function () {
         function myel(myclass = "") {
             if (!$.trim($('#inputme').val()) == '') {
                 let vall = $('#inputme').val()
-                
+                    , newH3 = $('<h3> </h3>')
+                    , newdiv = $('<div class="minibox" > </div')
+                    , bigdiv = $(`<div ${myclass} > </div`)
+                $(newH3).append(vall)
+                $(newdiv).append(newH3)
+                $(bigdiv).append(newdiv)
+                return bigdiv
             }
             else{
                 return
             }
         }
+        $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
         $("#inpute").val("")
      });
