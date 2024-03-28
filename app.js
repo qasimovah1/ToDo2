1ae8b519726f0f0d7dfcd9472ce418a54ff65e39
humay qasimova <hqasimova472@gmail.com>
2024-03-28 10:44:24 +0000

    demo

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 "use strict";
 $(document).ready(function () {
 
-     $("#me").click(function (e) { 
+    $("#me").click(function (e) {
         e.preventDefault();
-        function myel(myclass = "") {
-            if (!$.trim($('#inputme').val()) == '') {
-                let vall = $('#inputme').val()
-                    , newH3 = $('<h3> </h3>')
-                    , newdiv = $('<div class="minibox" > </div')
-                    , bigdiv = $(`<div ${myclass} > </div`)
-                $(newH3).append(vall)
-                $(newdiv).append(newH3)
-                $(bigdiv).append(newdiv)
-                return bigdiv
-            }
-            else{
-                return
-            }
+        function myel(myclass = '') {
+            let vall = $('#inputme').val()
+            
         }
-        $("#messageyou").append(myel())
-        $("#messageme").append(myel('class="end"'))
-        $("#inpute").val("")
-     });
+    });
 });
