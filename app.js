cd947fb4c34c8891f9b2493c1ca286d656eb8fe6
humay qasimova <hqasimova472@gmail.com>
2024-03-29 19:57:08 +0000

    demo

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 "use strict";
 $(document).ready(function () {
 
+
+
+
     $("#me").click(function (e) {
         e.preventDefault();
-        function myel(myclass = '') {
-            let vall = $('#inputme').val()
-            
+        function myel(myclass = "") {
+            if (!$.trim($('#inputme').val()) == "") {
+                let vall = $('#inputme').val()
+                
+            }
         }
+        
     });
 });
