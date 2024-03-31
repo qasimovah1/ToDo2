da9ddddf35dfe5ab8a5e6d0a3d1f238ca358bc36
humay qasimova <hqasimova472@gmail.com>
2024-03-31 18:25:31 +0000

    demo

--- app.js
+++ app.js
@@ -9,9 +9,23 @@ $(document).ready(function () {
         function myel(myclass = "") {
             if (!$.trim($('#inputme').val()) == "") {
                 let vall = $('#inputme').val()
-                
+                    ,newH3 = $('<h3> </h3>')
+                    ,newDiv = $('<div class="line"> </div>')
+                    ,bigDiv = $(`<div ${myclass} > </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiw)
+                return bigDiv
+            }
+            else {
+                return
             }
         }
-        
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class= "end"'))
+        $("#inputme").val("")
+
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messagme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
     });
 });
